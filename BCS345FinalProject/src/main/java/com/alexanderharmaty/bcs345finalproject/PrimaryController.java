14a4151e4bf617fde2c02c8ae344958f949aecb2
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2021-12-01 18:01:59 -0500

    project created

---
+++ BCS345FinalProject/src/main/java/com/alexanderharmaty/bcs345finalproject/PrimaryController.java
@@ -0,0 +1,13 @@
+package com.alexanderharmaty.bcs345finalproject;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+    
+}
---
+++ BCS345FinalProject/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.alexanderharmaty.bcs345finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.alexanderharmaty.bcs345finalproject to javafx.fxml;
+    exports com.alexanderharmaty.bcs345finalproject;
+}
+*/
